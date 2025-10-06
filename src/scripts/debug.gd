1b0d03fb582a9ff71432f473d333c8120d038229
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-10-06 14:44:16 +0200

    Clean up
    Remove game script from autoload
    Put Debug and Death Screen under a CanvasLayer in the main scene

--- src/scripts/debug.gd
+++ src/scripts/debug.gd
@@ -1,14 +1,16 @@
 extends Control
 
+@export var game: Game
+
 func _process(_delta: float):
-	$WorldSpeedLabel.text = "WORLD SPEED = x" + str(GM.worldspeed)
-	#$AnimationLabel.text = "currently playing " + GM.currentanim + "animation"
-	$ScoreLabel.text = "SCORE = " + str(GM.score)
-	$HighscoreLabel.text = "HIGHSCORE = " + str(GM.highscore)
+	$WorldSpeedLabel.text = "WORLD SPEED = x" + str(game.worldspeed)
+	#$AnimationLabel.text = "currently playing " + game.currentanim + "animation"
+	$ScoreLabel.text = "SCORE = " + str(game.score)
+	$HighscoreLabel.text = "HIGHSCORE = " + str(game.highscore)
 
 
 func _on_increase_button_pressed():
-	GM.worldspeed += GM.speedmodifier
+	game.worldspeed += game.speedmodifier
 
 func _on_decrease_button_pressed():
-	GM.worldspeed -= GM.speedmodifier
+	game.worldspeed -= game.speedmodifier
