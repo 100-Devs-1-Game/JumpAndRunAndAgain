7152b20a1ad9ecdaad4b04d7c379481047c0b9fc
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-10-07 01:29:33 +0200

    Update carrot.gd

--- src/scripts/carrot.gd
+++ src/scripts/carrot.gd
@@ -1,5 +1,10 @@
 extends Area2D
 
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-func _on_body_entered(body: Node2D) -> void:
-	hide()
+
+func reset():
+	animated_sprite.play("default")
+
+func _on_body_entered(_body: Node2D) -> void:
+	animated_sprite.play("empty")
