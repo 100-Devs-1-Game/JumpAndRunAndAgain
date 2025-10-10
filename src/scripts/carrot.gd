f7e7fcda694ac16143be42ea61718520f7083df3
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-10-10 04:13:42 +0200

    Activate and deactivate properly

--- src/scripts/carrot.gd
+++ src/scripts/carrot.gd
@@ -1,10 +1,43 @@
 extends Area2D
 
+@export var greyed_out: Color
+
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var loop_component: LoopComponent = $LoopComponent
+
+var can_collect := true
 
 
+func _ready() -> void:
+	var game: Game = get_tree().current_scene
+	assert(game)
+	
+	game.player_entered_new_loop.connect(activate.bind(true))
+	game.player_entered_current_loop.connect(activate.bind(true))
+	game.player_entered_previous_loop.connect(activate.bind(false))
+
+		
 func reset():
 	animated_sprite.play("default")
 
+
+func activate(flag: bool):
+	can_collect = flag
+	var color: Color
+	if flag:
+		color = Color.WHITE
+	else:
+		color = greyed_out
+
+	loop_component.set_property(animated_sprite, "modulate", color)
+
+
 func _on_body_entered(_body: Node2D) -> void:
+	if not can_collect:
+		return
+		
+	can_collect = false
 	animated_sprite.play("empty")
+	await get_tree().create_timer(0.2).timeout
+	animated_sprite.play("default")
+	loop_component.set_property(animated_sprite, "modulate", greyed_out)
