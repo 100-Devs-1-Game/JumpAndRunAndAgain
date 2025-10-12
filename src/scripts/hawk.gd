7bf71762768132871f71ca9464c876d868f9e952
Griiimon <25064539+Griiimon@users.noreply.github.com>
2025-10-12 09:56:29 +0200

    Rewrite movement

--- src/scripts/hawk.gd
+++ src/scripts/hawk.gd
@@ -6,14 +6,25 @@ extends CharacterBody2D
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 @onready var loop_component: LoopComponent = $LoopComponent
 
+var dir := -1
+
 
 func _ready() -> void:
 	assert(path_follow)
+	set_physics_process(false)
+	await loop_component.initialized
+	set_physics_process(true)
+	
 
 func _physics_process(delta: float) -> void:
-	path_follow.progress += move_speed * delta * ( 1 if animated_sprite.flip_h else -1 )
+	var prev_x: float = global_position.x
+	path_follow.progress += move_speed * delta * dir
+	if ( global_position.x > prev_x and not animated_sprite.flip_h )\
+		or ( global_position.x < prev_x and animated_sprite.flip_h ):
+			flip()
+			
 	if path_follow.progress_ratio <= 0 or path_follow.progress_ratio >= 1:
-		flip()
+		dir *= -1
 
 func flip():
 	loop_component.set_property(animated_sprite, "flip_h", not animated_sprite.flip_h)
